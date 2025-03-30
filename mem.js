58aa99844283ddf54567574362863da3f99796a3
Lokesh0224 <lokeshk20019@gmail.com>
2025-03-30 12:51:06 +0530

    bootstrap-brainly

---
+++ mem.js
@@ -0,0 +1,16 @@
+
+// npm init -y
+// npm install -d typescript
+// npx tsc --init
+
+
+
+
+// npm install express 
+
+// npm install -d @types/express
+//@ts-ignore
+// npm install mongoose
+// npm install jsonwebtoken 
+// npm install @types/jsonwebtoken 
+
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+const app= express()
+
+app.post('/api/v1/content', (req, res)=>{
+
+})
+
+app.post('/api/v1/content', (res, req)=>{
+
+})
+
+app.get('/api/v1/content', (res, req)=>{
+
+})
+
+app.delete('/api/v1/content', (res, req)=>{
+
+})
+
+app.post('/api/v1/content', (res, req)=>{
+
+})
+
+app.get('/api/v1/content', (res, req)=>{
+
+})
